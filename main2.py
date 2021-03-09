86a2440d33189140b54f83244d1b2e23a0e1d4b6
RYAN-FU-C <38944353+RYAN-FU-C@users.noreply.github.com>
2021-03-09 19:23:59 +0800

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,22 @@
+id1=1
+name1="牛仔裤"
+num1=21
+price1=99.9
+quanlity1 = "A+"
+type1 = "裤子"
+
+id2=2
+name2="运动服"
+num2=43
+price2=123
+quanlity2 = "S-"
+type2 = "上衣"
+
+
+print("---------------")
+print("   Jason衣服商城系统    ")
+print("--------------")
+print("编号  名称    销量    价格    质量   类型")
+print(id1,"  ",name1,"  ",num1,"  ",price1,"  ",quanlity1,"  ",type1)
+print(id2,"  ",name2,"  ",num2,"  ",price2,"  ",quanlity2,"  ",type2)
+print("总金额：",(num1*price1+num2*price2),"￥")
